3b1b18de527e6bf36e677e76e24c3010dee94424
DenisAFW <denis.afinogenow@yandex.ru>
2022-12-07 23:14:20 +0300

    Сменил параметр условия с && на || и отформалтировал

--- src/Numbers.java
+++ src/Numbers.java
@@ -8,12 +8,12 @@ public class Numbers {
         int[] arr1 = {5, 1, 4, 3, 9};
         int[] arr2 = {1, 5, 4, 8, 3};
 
-        divOf(arr1,arr2);
+        divOf(arr1, arr2);
 
     }
 
-    public static int[] divOf(int[] arr1, int[] arr2){
-        if (arr1 == null && arr2 == null) {
+    public static int[] divOf(int[] arr1, int[] arr2) {
+        if (arr1 == null || arr2 == null) {
             throw new RuntimeException("Массив не должен быть равен null!");
         }
         if (arr1.length != arr2.length) {
@@ -22,8 +22,8 @@ public class Numbers {
 
         int[] arr3 = new int[arr1.length];
 
-        for(int i = 0; i < arr1.length; i++){
-            if(arr2[i] == 0){
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr2[i] == 0) {
                 throw new RuntimeException("Делитель не должен быть равен 0!");
             }
             arr3[i] = arr1[i] / arr2[i];
